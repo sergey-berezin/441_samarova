f8c00857b5c116618dd45fde77ee1ebaebe1b0e1
s02180519 <monulaqi@mail.ru>
2021-11-17 00:19:26 +0300

    Data Base

---
+++ Task3/Transfer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    /// <summary>
+    /// Универсальный пакет для пересылки любых данных 
+    /// </summary>
+    public class Transfer 
+    {
+        public byte[] image { get; set; }
+
+        public string TypeName { get; set; }
+
+        public byte[] rectangle { get; set; }
+    }
+}
